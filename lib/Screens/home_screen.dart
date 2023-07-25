2c9461aec6a9c032d014624ff7ade75ee3ad15a5
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-07-26 02:30:49 +0800

    finish UI

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,199 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 100,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        title: Text(
+          'Basket Ball Counter',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 50.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.c,
+              children: [
+                Text(
+                  'Team A',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.amber),
+                      minimumSize: MaterialStateProperty.all(Size(80, 30))),
+                  onPressed: () {},
+                  child: Text(
+                    '1 point',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(80, 30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '2 points',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(80, 30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '3 points',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          VerticalDivider(
+            thickness: 2,
+            indent: 50.0,
+            endIndent: 50.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Team A',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.amber),
+                      minimumSize: MaterialStateProperty.all(Size(80, 30))),
+                  onPressed: () {},
+                  child: Text(
+                    '1 point',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(80, 30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '2 points',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(80, 30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '3 points',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
